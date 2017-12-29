6b647ed1fe338e2231983eb5552b6f9c6ae0cf3c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-29 14:18:19 +0000

    Fri Dec 29 14:18:19 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,11 +1,16 @@
 package befaster.solutions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class Checkout {
+
+    private static final String NAME = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
     /**
      * Method is responsible to calculate item price
@@ -17,7 +22,7 @@ public class Checkout {
 
         Map<Character, Integer> itemCounts = new HashMap<>();
         for (char item : skus.toCharArray()) {
-            if (item == 'A' || item == 'B' || item == 'C' || item == 'D' || item == 'E' || item == 'F') {
+            if (NAME.contains(String.valueOf(item))) {
                 Integer frequency = itemCounts.get(item);
                 itemCounts.put(item, (Objects.isNull(frequency) ? 1 : frequency + 1));
             } else
@@ -34,13 +39,76 @@ public class Checkout {
      * @return total price.
      */
     private static Integer priceCalculation(Map<Character, Integer> itemCounts) {
-
+        Integer itemCount;
         calculateOffer(itemCounts);
         Set<Character> characters = itemCounts.keySet();
         int totalPrice = 0;
 
         for (char item : characters) {
-            if (item == 'A') {
+            switch (item) {
+                case 'A':
+                    itemCount = itemCounts.get(item);
+                    if (itemCount >= 5) {
+                        totalPrice += (itemCount / 5) * 200;
+                        itemCount = itemCount % 5;
+                    }
+                    if (itemCount >= 3) {
+                        totalPrice += (itemCount / 3) * 130;
+                        itemCount = itemCount % 3;
+                    }
+                    if (itemCount <= 2) {
+                        totalPrice += itemCount * 50;
+                    }
+                    break;
+                case 'B':
+                    itemCount = itemCounts.get(item);
+                    totalPrice += ((itemCount % 2) * 30) + ((itemCount / 2) * 45);
+                    break;
+                case 'C':
+                    itemCount = itemCounts.get(item);
+                    totalPrice += itemCount * 20;
+                    break;
+                case 'D':
+                    itemCount = itemCounts.get(item);
+                    totalPrice += itemCount * 15;
+                    break;
+                case 'E':
+                    itemCount = itemCounts.get(item);
+                    totalPrice += itemCount * 40;
+                    break;
+                case 'F':
+                    itemCount = itemCounts.get(item);
+                    if (itemCount <= 2) {
+                        totalPrice += itemCount * 10;
+                    } else {
+                        int rem = itemCount % 3;
+                        int div = itemCount / 3;
+                        totalPrice += ((div * 2) + rem) * 10;
+                    }
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                case 'J':
+                case 'K':
+                case 'L':
+                case 'M':
+                case 'N':
+                case 'O':
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                case 'T':
+                case 'U':
+                case 'V':
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+
+            }
+           /* if (item == 'A') {
                 Integer itemCount = itemCounts.get(item);
                 if (itemCount >= 5) {
                     totalPrice += (itemCount / 5) * 200;
@@ -75,7 +143,7 @@ public class Checkout {
                     int div = itemCount / 3;
                     totalPrice += ((div * 2) + rem) * 10;
                 }
-            }
+            }*/
         }
         return totalPrice;
     }
